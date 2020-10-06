09eddf421b743c9005ec8cfb5bd5e9807bdd5893
Alan Chandler <alan@chandlerfamily.org.uk>
2020-10-06 16:43:24 +0100

    Add testing to the  repository

---
+++ tests/location.test.js
@@ -0,0 +1,120 @@
+/**
+@licence
+    Copyright (c) 2020 Alan Chandler, all rights reserved
+
+    This file is part of Distribted Router.
+
+    Distribted Router is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Distribted Router is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Distribted Router.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+
+
+import {connectUrl, disconnectUrl,LocationAltered} from '../location.js';
+import {RouteChanged} from '../route.js';
+
+let originalLocation, route;
+
+const testFunctions = {
+  setLocation: function (url) {
+    window.history.pushState({}, '', url);
+    window.dispatchEvent(new LocationAltered());
+  },
+  routeChanged: function (r) {
+    route = r;
+  }
+};
+
+
+describe('location module', function() {
+  let spy;
+  beforeEach(function() {
+
+    originalLocation = window.location.href;
+    spy = jest.spyOn(testFunctions, 'routeChanged');
+  });
+  afterEach(function() {
+    spy.mockRestore();
+    window.history.replaceState({}, '', originalLocation);
+  });
+  describe('connectUrl', function() {
+    it('adds event listeners for various events', function() {
+
+      const el = jest.spyOn(window, 'addEventListener');
+      connectUrl(testFunctions.routeChanged);
+      expect(el).toHaveBeenCalledWith('popstate',expect.anything());
+      expect(el).toHaveBeenCalledWith('hashchange', expect.anything());
+      expect(el).toHaveBeenCalledWith('location-altered', expect.anything());
+      expect(el).toHaveBeenCalledWith('route-changed', expect.anything());
+      expect(el.mock.calls[1][1]).toEqual(el.mock.calls[0][1]);
+      expect(el.mock.calls[2][1]).toEqual(el.mock.calls[0][1]);
+      el.mockRestore();
+    });
+    it('calls the callback function after Promise.resolving', async function() {
+      connectUrl(testFunctions.routeChanged);
+      expect(spy).not.toHaveBeenCalled();
+      await Promise.resolve();
+      expect(testFunctions.routeChanged).toHaveBeenCalled();;
+    });
+  });
+  describe('Responding to location-changed events', function(){
+    beforeEach(async function() {
+      connectUrl(testFunctions.routeChanged);
+      await Promise.resolve();
+    });
+
+    it('calls the callback function with route with current path ', function(){
+      testFunctions.setLocation('/test');
+      expect(route.path).toEqual('/test');
+      expect(route.active).toBeTruthy();
+      expect(route.segment).toEqual(0);
+    });
+    it('expect search string changes to reflect in route', function() {
+      testFunctions.setLocation('/test?date=20160101&staff=2');
+      expect(route.query).toEqual({date: 20160101, staff: 2});
+    });
+    it('reflect changes to route path to url', function() {
+      window.dispatchEvent(new RouteChanged({
+        segment: 0,
+        path: '/reports/bydate/20161231'
+      }));
+      expect(window.location.pathname).toEqual('/reports/bydate/20161231');
+    });
+    it('reflect query string changes to route.query', function() {
+      window.dispatchEvent(new RouteChanged({
+        query:{firstname: 'Joe', lastname: 'Bloggs'}
+      }));
+      expect(window.location.search).toEqual('?firstname=Joe&lastname=Bloggs');
+    });
+    it('setting query to null object in route should clear query string', function() {
+      window.dispatchEvent(new RouteChanged({
+        query:{firstname: 'Joe', lastname: 'Bloggs'}
+      }));
+      window.dispatchEvent(new RouteChanged({
+        query:{}
+      }));
+      expect(window.location.search).toEqual('');
+    });
+
+  });
+  it('disconnectUrl disconnects from all the events', function() {
+    const el = jest.spyOn(window, 'removeEventListener');
+    disconnectUrl();
+    expect(el).toHaveBeenCalledWith('popstate', expect.anything());
+    expect(el).toHaveBeenCalledWith('hashchange', expect.anything());
+    expect(el).toHaveBeenCalledWith('location-altered', expect.anything());
+    expect(el).toHaveBeenCalledWith('route-changed', expect.anything());
+  });
+
+});
---
+++ tests/route.test.js
@@ -0,0 +1,217 @@
+/**
+    @licence
+    Copyright (c) 2019 Alan Chandler, all rights reserved
+
+    This file is part of PASv5, an implementation of the Patient Administration
+    System used to support Accuvision's Laser Eye Clinics.
+
+    PASv5 is licenced to Accuvision (and its successors in interest) free of royality payments
+    and in perpetuity in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+    implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. Accuvision
+    may modify, or employ an outside party to modify, any of the software provided that
+    this modified software is only used as part of Accuvision's internal business processes.
+
+    The software may be run on either Accuvision's own computers or on external computing
+    facilities provided by a third party, provided that the software remains soley for use
+    by Accuvision (or by potential or existing customers in interacting with Accuvision).
+*/
+
+
+import Route from '../route.js';
+
+let router;
+
+describe('route', function() {
+  beforeEach(function() {
+    router = new Route('/:page');
+  });
+  afterEach(function() {
+  });
+  it('instantiates itself', function() {
+    expect(router.constructor.name).toEqual('Route');
+  });
+  it('fails if route path does not start with /', function() {
+    expect(router.routeChange.bind(
+      router,
+      {
+        path: 'reports/',
+        active: true,
+        params: {},
+        query: {},
+        segment:0
+      }
+    )).toThrow();
+  });
+  it('match a simple route', async function() {
+    const route = router.routeChange({path: '/reports', active: true, params: {}, query: {}, segment:0});
+    expect(route).toStrictEqual(
+      {path: '/', active: true, params: {page: 'reports'}, query: {}, segment:1});
+  });
+  it('match a segment only if ifMatched allows a match', function(){
+    const router = new Route('/:page', 'section:management');
+    const route  = router.routeChange({
+      path: '/reports',
+      active: true,
+      params: {section: 'management'},
+      query: {},
+      segment:1
+    });
+    expect(route).toStrictEqual(
+      {path: '/', active: true, params: {page: 'reports'}, query: {}, segment:2});
+  });
+  it('doesnt match if ifMatched shows wrong match', function() {
+    const router = new Route('/:page','section:nonstandard');
+    const route = router.routeChange({
+      path: '/reports',
+      active: true,
+      params: {section: 'management'},
+      query: {},
+      segment:1
+    });
+    expect(route.active).toBeFalsy();
+  });
+  it('match home route', function() {
+    const route = router.routeChange({path: '/', active: true, params: {}, query: {}, segment:0});
+    expect(route).toStrictEqual(
+      {path: '', active: true, params: {page: ''}, query: {}, segment:1});
+  });
+  it('passes remainder of path after match to out-route', function() {
+    const route = router.routeChange({path: '/reports/bydate', active: true, params: {}, query: {}, segment:0});
+    expect(route.path).toEqual('/bydate');
+  });
+  it('out route not active unless in-route active', function() {
+    const route = router.routeChange({path: '/reports', active: false, params: {}, query: {}, segment:0});
+    expect(route.active).toBeFalsy();
+  });
+  it('out route path has no "/" if match has trailing "/"', function() {
+    const router = new Route('/:page/');
+    const route = router.routeChange({path: '/reports', active: true, params: {}, query: {}, segment: 0});
+    expect(route.path).toEqual('');
+  });
+  it('in route with empty string for path will not match', function() {
+    const route = router.routeChange({path: '', active: true, params: {}, query: {}, segment: 0});
+    expect(route.active).toBeFalsy();
+  });
+  it('if matched parameter is all digits, params provides a number', function() {
+    const route = router.routeChange({path: '/20160101/staff', active: true, params: {}, query: {}, segment:0});
+    expect(route.params.page).toEqual(20160101);
+    expect(route.params.page).not.toEqual('20160101');
+  });
+  it('if matched parameter is all digits with a leading minus, params provides a number', function() {
+    const route = router.routeChange({path: '/-500/staff', active: true, params: {}, query: {}, segment: 0});
+    expect(route.params.page).toEqual(-500);
+    expect(route.params.page).not.toEqual('-500');
+  });
+  it('if matched parameter is all digits and non leading minus, params provides a string', function() {
+    let route = router.routeChange({path: '/5-01/staff', active: true, params: {}, query: {}, segment: 0});
+    expect(route.params.page).not.toEqual(501);
+    expect(route.params.page).not.toEqual(-501);
+    expect(route.params.page).toEqual('5-01');
+    route = router.routeChange({path: '/staff', active: true, params: {}, query: {}, segment: 0});
+    expect(route.params.page).toEqual('staff');
+  });
+  it('query parameters passed downwards', function() {
+    const route = router.routeChange({
+      path: '/reports',
+      active: true,
+      params: {},
+      query: {date: '20160101'},
+      segment:0
+    });
+    expect(route.query).toEqual({date: '20160101'});
+  });
+  describe('virtual query param and connection setters generate events', function() {
+    let location;
+    function routeUpdated(e) {
+      location = e.changed;
+    }
+    beforeEach(function(){
+      location = {};
+      window.addEventListener('route-changed', routeUpdated);
+    });
+    afterEach(function() {
+      window.removeEventListener('route-changed', routeUpdated);
+    });
+    it('query parameters passed upwards provided outRoute is active', function() {
+      router.routeChange({path: '/reports', active: true, params: {}, query: {}, segment: 0});
+      router.query = {date: '20161231'};
+      expect(location.query).toEqual({date: '20161231'});
+      expect(location.path).not.toBeDefined();
+    });
+    it('query parameters not passed upwards if outRoute Not Active', function() {
+      router.query = {date: '20161231'};
+      expect(location.query).not.toBeDefined();
+    });
+    describe('parameter setter', function() {
+      beforeEach(function() {
+        router.routeChange({path: '/reports/bydate', active: true, params: {}, query: {}, segment: 0});
+      });
+      it('changes in parameters effect in-route path', function() {
+        router.params = {page: 'query'};
+        expect(location.segment).toEqual(0);
+        expect(location.path).toEqual('/query/bydate');
+      });
+      it('changes to a single parameter effect in-route path', function() {
+        const router = new Route('/:page/:sub');
+        router.routeChange({path: '/reports/bydate', active: true, params: {}, query: {}, segment: 0});
+        router.params =  {page:'query'};
+        expect(location.path).toEqual('/query/bydate');
+      });
+      it('first optional parameters to \'\' will result in inRoute path of /', async function() {
+        const router = new Route('/:page/:sub');
+        router.routeChange({path: '/reports/bydate', active: true, params: {}, query: {}, segment: 0});
+        router.params = {page: ''};
+        expect(location.path).toEqual('/');
+      });
+      it('early optional parameters kept in path if latter empty', function() {
+        const router = new Route('/:page/:sub');
+        router.routeChange({path: '/reports/bydate', active: true, params: {}, query: {}, segment: 0});
+        router.params = {sub: ''};
+        expect(location.path).toEqual('/reports');
+      });
+      it('first optional paramters to null will result in inRoute path of /', function() {
+        router.params = {page: null};
+        expect(location.path).toEqual('/');
+      });
+      it('not passing a string, number or null as a parameter key, will throw an error', function() {
+        expect(() => {
+          router.params = {page: {}};
+        }).toThrow();
+      });
+    });
+    describe('connections setter', function() {
+      beforeEach(function() {
+        router = new Route('/full');
+      });
+      it('if preroute is not active, nothing happens', function() {
+        router.routeChange({path: '/reports', active: false, params: {}, query: {}, segment:0});
+        router.connection = true;
+        expect(location).toEqual({});
+        router.connection = false;
+        expect(location).toEqual({});
+      });
+      it('if outroute is active and we request active nothing happens', function() {
+        router.routeChange({path: '/full', active: true, params: {}, query: {}, segment:0});
+        router.connection = true;
+        expect(location).toEqual({});
+      });
+      it('if outroute is not active and we request active, it generates a new location', function() {
+        router.routeChange({path: '/empty', active: true, params: {}, query: {}, segment:0});
+        router.connection = true;
+        expect(location).toEqual({path:'/full', segment:0});
+      });
+      it('if outroute is not active and we request not active nothing happens', function() {
+        router.routeChange({path: '/empty', active: true, params: {}, query: {}, segment:0});
+        router.connection = false;
+        expect(location).toEqual({});
+      });
+      it('if outroute is active and we request it not active it generates a new location', function() {
+        router.routeChange({path: '/full', active: true, params: {}, query: {}, segment:0});
+        router.connection = false;
+        expect(location).toEqual({path:'/', segment:0});
+
+      });
+    });
+  });
+
+});
